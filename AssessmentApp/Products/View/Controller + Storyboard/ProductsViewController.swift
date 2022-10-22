d7d0994ddbcbe761da114fb37874621474d367d1
Salwa Hegazy <salwa.h@genie-soft.com>
2022-10-22 03:04:57 +0200

    set tableview data , indicatorLoader , remove unUsed storyboard and add some changes

--- AssessmentApp/Products/View/Controller + Storyboard/ProductsViewController.swift
+++ AssessmentApp/Products/View/Controller + Storyboard/ProductsViewController.swift
@@ -23,12 +23,52 @@ class ProductsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
-        
+        subscribeToLoading()
+        subscribeToResponse()
+        subscribeToProductSelection()
     }
     
     //MARK: - Methods
     func setupTableView() {
        tableView.register(UINib(nibName: productsTableViewCell, bundle: nil), forCellReuseIdentifier: productsTableViewCell)
     }
-
+    
+    func subscribeToLoading() {
+        productsViewModel.loadingBehavior.subscribe(onNext: { (isLoading) in
+            if isLoading {
+                self.showIndicator(withTitle: "", and: "")
+            } else {
+                self.hideIndicator()
+            }
+        }).disposed(by: disposeBag)
+    }
+    
+    func subscribeToResponse() {
+        productsViewModel.productsModelObservable
+            .bind(to: tableView
+                .rx
+                .items(cellIdentifier: productsTableViewCell,
+                       cellType: ProductsTableViewCell.self)) { row, product, cell in
+                        print(row)
+                        
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    func subscribeToProductSelection() {
+        Observable
+            .zip(tableView.rx.itemSelected, tableView.rx.modelSelected(ProductsModel.self))
+            .bind { [weak self] selectedIndex, product in
+                
+                print(selectedIndex, product.values)
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    func getProducts() {
+        productsViewModel.getProductsData()
+    }
+    
 }
+
+ 
